d08832175cb35c326de78aa6c501e23d4bc4aaec
lexy5251 <xixi5251@gmail.com>
2018-10-30 18:30:36 +0000

    some functions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,38 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+  let max =0;
+  let maxChar = "";
+  for (let i of str) {
+    if(obj[i]) {
+      obj[i] ++;
+    }else {
+      obj[i] = 1;
+    }
+  }
+
+ for (let char in obj) {
+   if (obj[char] > max) {
+    max = obj[char];
+    maxChar = char;
+   }
+ }
+  return maxChar;
+
+
+}
 
 module.exports = maxChar;
+
+
+
+// const string = "Hello World";
+// const arr = string.split("");
+// const obj = arr.reduce((counter, letter) => {
+//   counter[letter] ? counter[letter] ++ : counter[letter] = 1;
+//   return counter;
+// }, {})
+
+// console.log(obj)
