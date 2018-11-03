feaf4cab706abafb4538efb0cb712afa6f54a17d
lexy5251 <xixi5251@gmail.com>
2018-11-03 18:14:31 +0000

    section 12

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,21 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+      if (j <= i) {
+        row += "#";
+      } else {
+        row += " ";
+      }
+    }
+      console.log(row);
+  }
+
+
+}
 
 module.exports = steps;
